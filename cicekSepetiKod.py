85b8ef81642e6444038e5d7927123a953d651455
Ahmet Yanık <73365349+Ahmetyanikk@users.noreply.github.com>
2022-11-03 17:15:29 +0300

    Update cicekSepetiKod.py

--- cicekSepetiKod.py
+++ cicekSepetiKod.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-
-dfHam = pd.read_excel(r'C:\Users\\Ahmet Yanık\\PycharmProjects\pythonProject\Abiye Ayakkabı-13606-2014020.xlsx')
+filename="Fantezi Aksesuar-14935-2014168"
+dfHam = pd.read_excel(r'C:\Users\\Ahmet Yanık\\PycharmProjects\pythonProject\Tesettür Namaz Elbise-15031-2014205.xlsx')
 
 #['Sıra No','PKProductId','ProductCode','Name','Linkler','İç Giyim Yaka','Malzeme','Durum','Kaynak']
 url="https://www.ciceksepeti.com/"
@@ -14,8 +14,11 @@ for i in range(0,len(dfHam['PKProductId'])):
     dfHam.at[i, 'Link']=url+dfHam.iloc[row,3]
     dfHam.at[i, 'Sıra No'] = i+1
 
+list1=[len(dfHam.columns)-1]
+for i in range(1,len(dfHam.columns)-1):
+    list1.append(i)
 
-dfHam=dfHam.iloc[:, [11,1,2,3,4,5,6,7,8,9,10]]
+dfHam=dfHam.iloc[:, list1]
 dfHam['Durum'] = pd.Series(dtype='str')
 dfHam['Kaynak'] = pd.Series(dtype='str')
-dfHam.to_excel('Abiye Ayakkabı-13606-2014020_Final.xlsx',index=False)
+dfHam.to_excel('Tesettür Namaz Elbise-15031-2014205_Final.xlsx',index=False)
