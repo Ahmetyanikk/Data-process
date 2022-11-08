42a7778202a0205d7907d3d1590bccc469efd4bb
Ahmet Yanık <73365349+Ahmetyanikk@users.noreply.github.com>
2022-11-08 15:09:58 +0300

    Update cicekSepetiKod.py

--- cicekSepetiKod.py
+++ cicekSepetiKod.py
@@ -1,7 +1,7 @@
 import pandas as pd
-
+import glob
 filename="Fantezi Aksesuar-14935-2014168"
-dfHam = pd.read_excel(r'C:\Users\\Ahmet Yanık\\PycharmProjects\pythonProject\Tesettür Namaz Elbise-15031-2014205.xlsx')
+dfHam = pd.read_excel(r'C:\Users\\Ahmet Yanık\\PycharmProjects\pythonProject\\Tesettür Namaz Elbise-15031-2014205.xlsx')
 
 #['Sıra No','PKProductId','ProductCode','Name','Linkler','İç Giyim Yaka','Malzeme','Durum','Kaynak']
 url="https://www.ciceksepeti.com/"
@@ -13,6 +13,7 @@ row=0
 for i in range(0,len(dfHam['PKProductId'])):
     dfHam.at[i, 'Link']=url+dfHam.iloc[row,3]
     dfHam.at[i, 'Sıra No'] = i+1
+    row=row+1
 
 list1=[len(dfHam.columns)-1]
 for i in range(1,len(dfHam.columns)-1):
