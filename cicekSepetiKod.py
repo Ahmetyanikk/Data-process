1af30139bad5cdddb7d285c77a7667b4f30cbef3
Ahmet Yanık <73365349+Ahmetyanikk@users.noreply.github.com>
2022-11-08 15:17:25 +0300

    Update cicekSepetiKod.py

--- cicekSepetiKod.py
+++ cicekSepetiKod.py
@@ -1,25 +1,30 @@
 import pandas as pd
 import glob
 filename="Fantezi Aksesuar-14935-2014168"
-dfHam = pd.read_excel(r'C:\Users\\Ahmet Yanık\\PycharmProjects\pythonProject\\Tesettür Namaz Elbise-15031-2014205.xlsx')
 
+inputpath = r"C:\\Users\\\Ahmet Yanık\\\PycharmProjects\\pythonProject\\venv\\CicekSepetiInput\\"
+outputpath = r"C:\\Users\\\Ahmet Yanık\\\PycharmProjects\\pythonProject\\venv\\CicekSepetiOutput\\"
+excelpath = glob.glob(inputpath+"*.xlsx")
 #['Sıra No','PKProductId','ProductCode','Name','Linkler','İç Giyim Yaka','Malzeme','Durum','Kaynak']
 url="https://www.ciceksepeti.com/"
+for excel in excelpath:
+    filename = excel.split('\\')[-1][:-5]
+    dfmev = pd.read_excel(excel)
+    dfmev.drop('CreatedOn', inplace=True, axis=1)
+    dfmev=dfmev.sort_values('Name',ignore_index=True)
+    row = 0
+    for i in range(0, len(dfmev['PKProductId'])):
+        dfmev.at[i, 'Link'] = url + dfmev.iloc[row, 3]
+        dfmev.at[i, 'Sıra No'] = i + 1
+        row = row + 1
+    list1 = [len(dfmev.columns) - 1]
+    for i in range(1, len(dfmev.columns) - 1):
+        list1.append(i)
 
-dfHam.drop('CreatedOn', inplace=True, axis=1)
-dfHam=dfHam.sort_values('Name',ignore_index=True)
+    dfmev = dfmev.iloc[:, list1]
+    dfmev['Durum'] = pd.Series(dtype='str')
+    dfmev['Kaynak'] = pd.Series(dtype='str')
+    dfmev.to_excel(outputpath+f'{filename}'+".xlsx", index=False)
 
-row=0
-for i in range(0,len(dfHam['PKProductId'])):
-    dfHam.at[i, 'Link']=url+dfHam.iloc[row,3]
-    dfHam.at[i, 'Sıra No'] = i+1
-    row=row+1
 
-list1=[len(dfHam.columns)-1]
-for i in range(1,len(dfHam.columns)-1):
-    list1.append(i)
 
-dfHam=dfHam.iloc[:, list1]
-dfHam['Durum'] = pd.Series(dtype='str')
-dfHam['Kaynak'] = pd.Series(dtype='str')
-dfHam.to_excel('Tesettür Namaz Elbise-15031-2014205_Final.xlsx',index=False)
